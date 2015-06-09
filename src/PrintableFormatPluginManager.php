881be41fa3f38afc6bb13c4cc04f6a263bf08326
alok <iamalok185@gmail.com>
2015-06-09 14:10:11 +0530

    removed some more mistakes

--- src/PrintableFormatPluginManager.php
+++ src/PrintableFormatPluginManager.php
@@ -30,7 +30,7 @@ class PrintableFormatPluginManager extends DefaultPluginManager {
    *   An object that implements \Traversable which contains the root paths
    *   keyed by the corresponding namespace to look for plugin implementations.
    * @param \Drupal\Core\Config\ConfigFactory $config
-   *  The config factory service.
+   *   The config factory service.
    * @param \Drupal\Core\Extension\ModuleHandlerInterface $module_handler
    *   The module handler to invoke the alter hook with.
    */
@@ -46,4 +46,5 @@ class PrintableFormatPluginManager extends DefaultPluginManager {
     $configuration += (array) $this->config->get('printable.format')->get($plugin_id);
     return parent::createInstance($plugin_id, $configuration);
   }
+
 }
