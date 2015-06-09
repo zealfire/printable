59a5bcbcde5d76559b601f1b2fa46845c1ec57bb
alok <iamalok185@gmail.com>
2015-06-09 11:40:25 +0530

    removed debug messages

--- src/Plugin/PrintableFormatBase.php
+++ src/Plugin/PrintableFormatBase.php
@@ -2,28 +2,27 @@
 
 /**
  * @file
- * Contains \Drupal\hardcopy\Plugin\HardcopyFormatBase.
+ * Contains \Drupal\printable\Plugin\PrintableFormatBase.
  */
 
-namespace Drupal\hardcopy\Plugin;
+namespace Drupal\printable\Plugin;
 
 use Drupal\Core\Config\ConfigFactory;
 use Drupal\Core\Plugin\PluginBase;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
-use Drupal\hardcopy\LinkExtractor\LinkExtractorInterface;
+use Drupal\printable\LinkExtractor\LinkExtractorInterface;
 use Drupal\Core\Render\Element\Html;
-use Drupal\hardcopy\HardcopyCssIncludeInterface;
+use Drupal\printable\PrintableCssIncludeInterface;
 use Drupal\Core\Form\FormStateInterface;
 use Wa72\HtmlPageDom\HtmlPage;
 use Wa72\HtmlPageDom\HtmlPageCrawler;
- 
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Provides a base class for Filter plugins.
  */
-abstract class HardcopyFormatBase extends PluginBase implements HardcopyFormatInterface, ContainerFactoryPluginInterface {
+abstract class PrintableFormatBase extends PluginBase implements PrintableFormatInterface, ContainerFactoryPluginInterface {
 
   /**
    * The config factory service.
@@ -33,16 +32,16 @@ abstract class HardcopyFormatBase extends PluginBase implements HardcopyFormatIn
   protected $configFactory;
 
   /**
-   * Hardcopy CSS include manager.
+   * Printable CSS include manager.
    *
-   * @var \Drupal\hardcopy\HardcopyCssIncludeInterface
+   * @var \Drupal\printable\PrintableCssIncludeInterface
    */
-  protected $hardcopyCssInclude;
+  protected $printableCssInclude;
 
   /**
-   * Hardcopy link extractor.
+   * Printable link extractor.
    *
-   * @var \Drupal\hardcopy\LinkExtractor\LinkExtractorInterface
+   * @var \Drupal\printable\LinkExtractor\LinkExtractorInterface
    */
   protected $linkExtractor;
 
@@ -51,14 +50,14 @@ abstract class HardcopyFormatBase extends PluginBase implements HardcopyFormatIn
    *
    * @param \Drupal\Core\Config\ConfigFactory $config_factory
    *  The config factory service.
-   * @param \Drupal\hardcopy\HardcopyCssIncludeInterface $hardcopy_css_include
-   *  The hardcopy CSS include manager.
+   * @param \Drupal\printable\PrintableCssIncludeInterface $printable_css_include
+   *  The printable CSS include manager.
    */
-  public function __construct(array $configuration, $plugin_id, array $plugin_definition, ConfigFactory $config_factory, HardcopyCssIncludeInterface $hardcopy_css_include, LinkExtractorInterface $link_extractor) {
+  public function __construct(array $configuration, $plugin_id, array $plugin_definition, ConfigFactory $config_factory, PrintableCssIncludeInterface $printable_css_include, LinkExtractorInterface $link_extractor) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
 
     $this->configFactory = $config_factory;
-    $this->hardcopyCssInclude = $hardcopy_css_include;
+    $this->printableCssInclude = $printable_css_include;
     $this->linkExtractor = $link_extractor;
     $this->configuration += $this->defaultConfiguration();
   }
@@ -70,8 +69,8 @@ abstract class HardcopyFormatBase extends PluginBase implements HardcopyFormatIn
     return new static(
       $configuration, $plugin_id, $plugin_definition,
       $container->get('config.factory'),
-      $container->get('hardcopy.css_include'),
-      $container->get('hardcopy.link_extractor')
+      $container->get('printable.css_include'),
+      $container->get('printable.link_extractor')
     );
   }
 
@@ -108,7 +107,7 @@ abstract class HardcopyFormatBase extends PluginBase implements HardcopyFormatIn
    */
   public function setConfiguration(array $configuration) {
     $this->configuration = $configuration;
-    $this->configFactory->get('hardcopy.format')->set($this->getPluginId(), $this->configuration)->save();
+    $this->configFactory->get('printable.format')->set($this->getPluginId(), $this->configuration)->save();
   }
 
   /**
@@ -120,7 +119,6 @@ abstract class HardcopyFormatBase extends PluginBase implements HardcopyFormatIn
    * {@inheritdoc}
    */
   public function setContent(array $content) {
-    echo "<br>one<br>";
     $this->content = $content;
   }
 
@@ -128,48 +126,38 @@ abstract class HardcopyFormatBase extends PluginBase implements HardcopyFormatIn
    * {@inheritdoc}
    */
   public function getResponse() {
-    echo "<br>two<br>";
     return new Response($this->getOutput());
   }
+
   /**
-   * Build a render array of the content, wrapped in the hardcopy theme.
+   * Build a render array of the content, wrapped in the printable theme.
    *
    * @return array
    *  A render array representing the themed output of the content.
    */
-  protected function buildContent() {
-    echo "<br>five<br>";
+  protected function buildContent() 
     $build = array(
-      '#theme' => array('hardcopy__' . $this->getPluginId(), 'hardcopy'),
+      '#theme' => array('printable__' . $this->getPluginId(), 'printable'),
       '#header' => array(
-        '#theme' => array('hardcopy_header__' . $this->getPluginId(), 'hardcopy_header'),
+        '#theme' => array('printable_header__' . $this->getPluginId(), 'printable_header'),
       ),
       '#content' => $this->content,
       '#footer' => array(
-        '#theme' => array('hardcopy_footer__' . $this->getPluginId(), 'hardcopy_footer'),
+        '#theme' => array('printable_footer__' . $this->getPluginId(), 'printable_footer'),
       ),
       '#attached' => array(
         'library' => array(
           array('system', 'jquery'),
           array('system', 'drupal'),
-          array('hardcopy/css','drupal-hardcopy'),
-          array('hardcopy/js','script'),
+          array('printable/css','drupal-printable'),
+          array('printable/js','script'),
         ),
       ),
     );
 
-    if ($include_path = $this->hardcopyCssInclude->getCssIncludePath()) {
+    if ($include_path = $this->printableCssInclude->getCssIncludePath()) {
       $build['#attached']['css'][] = $include_path;
     }
-
-    // Eeeew. @todo remove this so we can unit test this method.
-    //system_page_build($build);
-    $build['#attached']['library'][] = 'hardcopy/drupal.hardcopy';
-    $build['#attached']['js'] = array(
-  'http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js' => array(
-    'type' => 'external',
-  ),
-);
     return $build;
   }
 
@@ -178,15 +166,12 @@ abstract class HardcopyFormatBase extends PluginBase implements HardcopyFormatIn
    * object.
    *
    * @return string
-   *  The HTML string representing the output of this hardcopy format.
+   *  The HTML string representing the output of this printable format.
    */
   protected function getOutput() {
-    echo "<br>three<br>";
     $content = $this->buildContent();
-    echo "<br>six<br>";
     $rendered_page = render($content);
-    echo "<br>ten<br>";
-   if ($this->configFactory->get('hardcopy.settings')->get('extract_links')) {
+   if ($this->configFactory->get('printable.settings')->get('extract_links')) {
       $rendered_page = $this->linkExtractor->extract((string) $rendered_page);
     }
     else {
