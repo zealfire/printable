68928a03a54281baf1e59e28faf04b184230bc70
alok <iamalok185@gmail.com>
2015-06-13 20:18:42 +0530

    added the link extract which removes and adds link in the page

---
+++ src/LinkExtractor/InlineLinkExtractor.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\printable\LinkExtractor\InlineLinkExtractor.
+ */
+
+namespace Drupal\printable\LinkExtractor;
+
+use Drupal\Core\Routing\UrlGeneratorInterface;
+use Drupal\printable\LinkExtractor\LinkExtractorInterface;
+use wa72\htmlpagedom\HtmlPageCrawler;
+
+/**
+ * Link extractor
+ */
+class InlineLinkExtractor implements LinkExtractorInterface {
+
+  /**
+   * The DomCrawler object.
+   *
+   * @var \Wa72\HtmlPageDom\HtmlPageCrawler
+   */
+  protected $crawler;
+
+  /**
+   * The URL generator service.
+   *
+   * @var \Drupal\Core\Routing\UrlGeneratorInterface
+   */
+  protected $urlGenerator;
+
+  /**
+   * Constructs a new InlineLinkExtractor object.
+   */
+  public function __construct(HtmlPageCrawler $crawler, UrlGeneratorInterface $url_generator) {
+    $this->crawler = $crawler;
+    $this->urlGenerator = $url_generator;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function extract( $string) {
+    $this->crawler->addContent($string);
+
+    $this->crawler->filter('a')->each(function(HtmlPageCrawler $anchor, $uri) {
+      $href = $anchor->attr('href');
+      // This method is deprecated, however it is the correct method to use here
+      // as we only have the path
+      $href = $this->urlGenerator->generateFromPath($href, array('absolute' => TRUE));
+      $anchor->append(' (' . $href . ')');
+    });
+
+    return (string) $this->crawler;
+  }
+
+  public function removeAttribute( $content, $attr) {
+    //echo ($content);
+    //echo "hola starts";
+    $this->crawler->addContent($content);
+    $this->crawler->filter('a')->each(function(HtmlPageCrawler $anchor, $uri) {
+      $anchor->removeAttribute('href');
+    });
+   return (string) $this->crawler;// $this->crawler->removeAttribute($string);
+    //return (string) $this->crawler;
+  }
+}
---
+++ src/LinkExtractor/LinkExtractorInterface.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\printable\LinkExtractor\LinkExtractorInterface
+ */
+
+namespace Drupal\printable\LinkExtractor;
+
+/**
+ * Defines an interface for extracting links from a string of HTMl.
+ */
+interface LinkExtractorInterface {
+
+  /**
+   * Highlight hrefs from links in the given HTML string.
+   *
+   * @param string $string
+   *   The HTML string to extract links from.
+   * @return string
+   *   The HTML string, with links highlighted.
+   */
+  public function extract( $string);
+
+  /**
+   * Remove href from links in the given HTML string.
+   *
+   * @param string $content
+   *   The HTML string to remove links from.
+   * @param  string $attr
+   *   The attribute which has to be removed from the link.
+   * @return string
+   *   The HTML string, with links removed.
+   */
+  public function removeAttribute( $content, $attr);
+  
+}
