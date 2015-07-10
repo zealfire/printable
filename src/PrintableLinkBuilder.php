3ae7d2e77395b5dc603343617ef6d3b3b3d67d1e
zealfire <iamalok185@gmail.com>
2015-07-11 01:03:44 +0530

    Removed unnecessary route interface.

--- src/PrintableLinkBuilder.php
+++ src/PrintableLinkBuilder.php
@@ -9,8 +9,8 @@ namespace Drupal\printable;
 
 use Drupal\printable\PrintableFormatPluginManager;
 use Drupal\Core\Config\ConfigFactory;
+use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Entity\EntityInterface;
-use Drupal\Core\Routing\UrlGeneratorInterface;
 use Drupal\Core\Url;
 
 /**
@@ -44,14 +44,11 @@ class PrintableLinkBuilder implements PrintableLinkBuilderInterface {
    *
    * @param \Drupal\Core\Config\ConfigFactory $config_factory
    *   The configuration factory service.
-   * @param \Drupal\Core\Routing\UrlGeneratorInterface $url_generator
-   *   The URL generator service.
    * @param \Drupal\printable\PrintableFormatPluginManager $printable_format_manager
    *   The printable format plugin manager.
    */
-  public function __construct(ConfigFactory $config_factory, UrlGeneratorInterface $url_generator, PrintableFormatPluginManager $printable_format_manager) {
+  public function __construct(ConfigFactoryInterface $config_factory, PrintableFormatPluginManager $printable_format_manager) {
     $this->configFactory = $config_factory;
-    $this->urlGenerator = $url_generator;
     $this->printableFormatManager = $printable_format_manager;
   }
 
@@ -66,7 +63,6 @@ class PrintableLinkBuilder implements PrintableLinkBuilderInterface {
         'title' => $definition['title'],
         'url' => Url::fromRoute('printable.show_format.' . $entity->getEntityTypeId(), array('printable_format' => $key, 'entity' => $entity->id())),
       );
-
       // Add target "blank" if the configuration option is set.
       if ($this->configFactory->get('printable.settings')->get('open_target_blank')) {
         if ($key == 'print' or !$this->configFactory->get('printable.settings')->get('save_pdf'))
